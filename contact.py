6997b72c9ae01b7eb4beb6b5f0de823dc7e07c22
Pavlov85 <wapxl@mail.ru>
2017-12-20 23:13:05 +0300

    Проведен рефакторинг кода. Созданы параметры и вспомогательынй класс

---
+++ contact.py
@@ -0,0 +1,23 @@
+class Contact:
+    def __init__(self, firsrname, middlename, nickname, title, lastname, company, adress, home, mobile, work,
+                       fax, email, email2, email3, homepage, byear, ayear, adress2, phone2, notes):
+        self.firstname = firsrname
+        self.middlename = middlename
+        self.nickname = nickname
+        self.title = title
+        self.lastname = lastname
+        self.company = company
+        self.adress = adress
+        self.home = home
+        self.mobile = mobile
+        self.work = work
+        self.fax = fax
+        self.email = email
+        self.email2 = email2
+        self.email3 = email3
+        self.homepage = homepage
+        self.byear = byear
+        self.ayear = ayear
+        self.adress2 = adress2
+        self.phone2 = phone2
+        self.notes = notes
