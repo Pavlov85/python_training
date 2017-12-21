03f84d04bfc513dc4d237f36a362a117b4e4426b
Pavlov85 <wapxl@mail.ru>
2017-12-21 22:36:48 +0300

    Оптимизирован код

--- test_add_group.py
+++ test_add_group.py
@@ -20,6 +20,7 @@ class test_add_group(unittest.TestCase):
         wd.get("http://localhost/addressbook/")
 
     def login(self, wd, username, password):
+        self.open_home_page(wd)
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
@@ -33,6 +34,7 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_link_text("groups").click()
 
     def create_group(self, wd, group):
+        self.open_groups_page(wd)
         # Создаем новую группу
         wd.find_element_by_name("new").click()
         # Заполняем поля группы
@@ -47,6 +49,7 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # Подтверждаем создание новой группы
         wd.find_element_by_name("submit").click()
+        self.return_to_group_page(wd)
 
     def return_to_group_page(self, wd):
         wd.find_element_by_link_text("group page").click()
@@ -56,20 +59,14 @@ class test_add_group(unittest.TestCase):
 
     def test_add_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
         self.create_group(wd, Group(name="asdd", header="asdsad", footer="asdas"))
-        self.return_to_group_page(wd)
         self.logout(wd)
 
     def test_add_empty_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
         self.create_group(wd, Group(name="", header="", footer=""))
-        self.return_to_group_page(wd)
         self.logout(wd)
 
     def tearDown(self):
