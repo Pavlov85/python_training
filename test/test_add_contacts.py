dff37d53a9fdec9f97bf1fc7442fc4079c6dfe28
Pavlov85 <wapxl@mail.ru>
2017-12-25 23:57:50 +0300

    Созданы помощники для входа\выхода, создания групп и контактов

--- test/test_add_contacts.py
+++ test/test_add_contacts.py
@@ -12,11 +12,11 @@ def tmp(request):
 
 
 def test_add_contact(tmp):
-    tmp.login(username="admin", password="secret")
-    tmp.create_contact(Contact(firstname="qqqqqqqq", middlename="wwwwwww", nickname="eeefdeeee", title="vvvvvvvvvv", lastname="eeeeeeeee", company="xccccccccc",
+    tmp.session.login(username="admin", password="secret")
+    tmp.contact.create_contact(Contact(firstname="qqqqqqqq", middlename="wwwwwww", nickname="eeefdeeee", title="vvvvvvvvvv", lastname="eeeeeeeee", company="xccccccccc",
                             adress="ffcvcxvcvcxvxcvx", home="23144124214", mobile="45565656678", work="56678678678", fax="67867868686",
                             email="wap@mail.ru", email2="trest@mail.ru", email3="big@mail.ru", homepage="http://wwwww.ru", byear="1985", ayear="2000",
                             adress2="sdfdsfsdfsdfsd", phone2="sdfsdfsdfsdfsdf", notes="sfsdfsdfdssdfsdfs"))
-    tmp.logout()
+    tmp.session.logout()
 
 
