9fef5165fd59a2be8a716aaabaf9d06a2bffc4f9
Pavlov85 <wapxl@mail.ru>
2018-01-03 18:55:05 +0300

    Добавлена валидация фикстуры

--- test/test_add_contacts.py
+++ test/test_add_contacts.py
@@ -3,11 +3,7 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="qqqqqqqq", middlename="wwwwwww", nickname="eeefdeeee", title="vvvvvvvvvv", lastname="eeeeeeeee", company="xccccccccc",
                                adress="ffcvcxvcvcxvxcvx", home="23144124214", mobile="45565656678", work="56678678678", fax="67867868686",
                                email="wap@mail.ru", email2="trest@mail.ru", email3="big@mail.ru", homepage="http://wwwww.ru", byear="1985", ayear="2000",
                                address2="sdfdsfsdfsdfsd", phone2="sdfsdfsdfsdfsdf", notes="sfsdfsdfdssdfsdfs"))
-    app.session.logout()
-
-
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -2,6 +2,4 @@ from model.group import Group
 
 
 def test_edit_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.edit(Group(name="xxxx", header="yyyyy", footer="zzzz"))
-    app.session.logout()
