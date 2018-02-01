a88ca0f55ca684d48ffadedf6f54943a0ad1b03b
Pavlov85 <wapxl@mail.ru>
2018-02-02 00:52:00 +0300

    Реализованы генераторы тестовых данных для групп и контаков, которые сохраняются в файлы json

---
+++ data/contacts.py
@@ -0,0 +1,18 @@
+from model.contact import Contact
+
+testdata = [
+    Contact(firstname="qqqqqqqq", middlename="wwwwwww", nickname="eeefdeeee", title="vvvvvvvvvv",
+            lastname="eeeeeeeee", company="xccccccccc",
+            adress="ffcvcxvcvcxvxcvx", home="23144124214", mobile="45565656678",
+            work="56678678678", fax="67867868686",
+            email="wap@mail.ru", email2="trest@mail.ru", email3="big@mail.ru",
+            homepage="http://wwwww.ru", byear="1985", ayear="2000",
+            address2="sdfdsfsdfsdfsd", phone2="sdfsdfsdfsdfsdf", notes="sfsdfsdfdssdfsdfs"),
+Contact(firstname="f1", middlename="m1", nickname="n1", title="t1",
+            lastname="l1", company="c1",
+            adress="ffcvcxvcvcxvxcvx", home="23144124214", mobile="45565656678",
+            work="56678678678", fax="67867868686",
+            email="wap@mail.ru", email2="trest@mail.ru", email3="big@mail.ru",
+            homepage="http://wwwww.ru", byear="1985", ayear="2000",
+            address2="sdfdsfsdfsdfsd", phone2="sdfsdfsdfsdfsdf", notes="sfsdfsdfdssdfsdfs")
+]
