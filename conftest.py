60884c5a97dbd9b35d674271c1996e35d2107ad6
Pavlov85 <wapxl@mail.ru>
2018-02-02 01:11:32 +0300

    Реализованы генераторы тестовых данных для групп и контаков, которые сохраняются в файлы json Реализована загрузка тестовых данных из файлов в формате JSON и параметризация тестов путём "внедрения" с pytest_generate_tests

--- conftest.py
+++ conftest.py
@@ -3,6 +3,7 @@ import json
 import os.path
 from fixture.application import Application
 import importlib
+import jsonpickle
 
 fixture = None
 target = None
@@ -49,4 +50,5 @@ def load_form_module(module):
   return importlib.import_module("data.%s" % module).testdata
 
 def load_from_json(file):
-    pass
+    with open (os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
+        return jsonpickle.decode(f.read())
