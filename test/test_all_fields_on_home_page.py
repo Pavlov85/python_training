7752f60f62637fa22f5a5eea825b0e4f4ebe81c3
Pavlov85 <wapxl@mail.ru>
2018-02-13 23:48:46 +0300

    Проверка информации о контактах на главной странице

--- test/test_all_fields_on_home_page.py
+++ test/test_all_fields_on_home_page.py
@@ -1,5 +1,10 @@
 import re
 
+def test_all_contacts_from_home_page_and_db(app, db):
+    contact_from_home_page = app.contact.get_contact_list()
+    contact_from_db = db.get_contact_list()
+    assert contact_from_home_page == contact_from_db
+
 def test_lastname_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
